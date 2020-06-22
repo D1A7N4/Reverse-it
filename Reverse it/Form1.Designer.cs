d90700728d50206482011167f5d620b8f3f5a568
Daniel Seavers <daniel.p.seavers@gmail.com>
2020-06-22 12:51:34 +0100

    Developed the program so it actually exists

--- Reverse it/Form1.Designer.cs
+++ Reverse it/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBX_Input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BTN_Reverse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reverse It";
+            // 
+            // TBX_Input
+            // 
+            this.TBX_Input.Location = new System.Drawing.Point(101, 114);
+            this.TBX_Input.Name = "TBX_Input";
+            this.TBX_Input.Size = new System.Drawing.Size(126, 20);
+            this.TBX_Input.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Input your string:";
+            // 
+            // BTN_Reverse
+            // 
+            this.BTN_Reverse.Location = new System.Drawing.Point(101, 155);
+            this.BTN_Reverse.Name = "BTN_Reverse";
+            this.BTN_Reverse.Size = new System.Drawing.Size(126, 30);
+            this.BTN_Reverse.TabIndex = 3;
+            this.BTN_Reverse.Text = "Reverse it!";
+            this.BTN_Reverse.UseVisualStyleBackColor = true;
+            this.BTN_Reverse.Click += new System.EventHandler(this.BTN_Reverse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(334, 225);
+            this.Controls.Add(this.BTN_Reverse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TBX_Input);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBX_Input;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BTN_Reverse;
     }
 }
 
--- Reverse it/Form1.cs
+++ Reverse it/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace Reverse_it
 {
     public partial class Form1 : Form
@@ -15,6 +14,34 @@ namespace Reverse_it
         public Form1()
         {
             InitializeComponent();
+        }
+        private void BTN_Reverse_Click(object sender, EventArgs e)
+        {
+            string output = "";
+            int vowels = 0;
+            int consonants = 0;
+            for (int i = 0; i < TBX_Input.Text.Length; i++)
+            {
+                output = TBX_Input.Text[i] + output;
+                if (TBX_Input.Text[i] == 'a' || TBX_Input.Text[i] == 'e' || TBX_Input.Text[i] == 'i' || TBX_Input.Text[i] == 'o' || TBX_Input.Text[i] == 'u')
+                {
+                    vowels += 1;
+                }
+                else
+                {
+                    consonants += 1;
+                }
+            }
+            MessageBox.Show(output);
+            MessageBox.Show("It has " + vowels + " vowels and " + consonants + " consonants.");
+            if (output == TBX_Input.Text)
+            {
+                MessageBox.Show("Your input is a palindrome.");
+            }
+            else
+            {
+                MessageBox.Show("Your input is not a palindrome.");
+            }
         }
     }
 }
